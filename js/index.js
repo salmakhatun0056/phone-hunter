bbcc1ea793d6cdc1cb41c54e3abc4e896547b915
salmakhatun12345e <ummesalma0056@gmail.com>
2022-02-28 21:32:40 +0600

    added-search-box

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log('hi')
+const searchClick = () => {
+    const searchBox = document.getElementById('search-box').value
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchBox}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
