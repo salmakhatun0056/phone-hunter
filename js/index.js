72d7478f38dc572100cec35523fa2701f990602d
salmakhatun12345e <ummesalma0056@gmail.com>
2022-03-01 16:43:25 +0600

    slice-handling-finish

--- js/index.js
+++ js/index.js
@@ -5,25 +5,29 @@ const error = document.getElementById('error')
 const searchClick = () => {
     main.innerHTML = ''
     error.innerText = ''
-    const searchBox = document.getElementById('search-box').value
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchBox}`
+    const searchBox = document.getElementById('search-box')
+    const searchBoxValue = searchBox.value
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchBoxValue}`
+    searchBox.value = ''
     fetch(url)
         .then(res => res.json())
         // .then(data => showPhoneDetails(data.data))
         .then((data) => {
-            console.log(data.data == false)
+            // console.log(data.data == false)
             if (data.data == false) {
-                error.innerText = 'plese type right phone name'
+                error.innerText = 'No Phone Found'
             }
             else {
                 showPhoneDetails(data.data)
             }
         })
 
+
 }
 const showPhoneDetails = (phones) => {
     // console.log(phones)
-    for (const phone of phones) {
+    const first20Phones = phones.slice(0, 20)
+    for (const phone of first20Phones) {
         console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col-lg-4')
@@ -34,8 +38,8 @@ const showPhoneDetails = (phones) => {
         <div class="card mx-auto" style="width: 18rem;">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-               <h5 class="card-title">${phone.phone_name}</h5>
-               <p class="card-text">${phone.brand}</p>
+               <h5 class="card-title">Phone-Name: ${phone.phone_name}</h5>
+               <p class="card-text">Brand: ${phone.brand}</p>
                <button onclick="phoneDetail('${phone.slug}')" href="#" class="btn btn-primary">See Detail </button>
             </div>
         </div>
@@ -60,7 +64,7 @@ const setDetail = (info) => {
         <div class="card mx-auto" style="width: 18rem;">
             <img src="${info.image}" class="card-img-top" alt="...">
             <div class="card-body">
-               <p id="noRelease" class="card-text">there is no realese date</p>
+               <p id="noRelease" class="card-text text-success">No release Date Found</p>
                <p class="card-text">Storage: ${info.mainFeatures.storage}</p>
                <p class="card-text">DisplaySize: ${info.mainFeatures.displaySize}</p>
                <p class="card-text">Sensors: ${info.mainFeatures.sensors}</p>
