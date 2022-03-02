f2cfeff197454c24c365588224e1d10a1116baf6
salmakhatun12345e <ummesalma0056@gmail.com>
2022-03-02 21:22:03 +0600

    end

--- js/index.js
+++ js/index.js
@@ -11,9 +11,7 @@ const searchClick = () => {
     searchBox.value = ''
     fetch(url)
         .then(res => res.json())
-        // .then(data => showPhoneDetails(data.data))
         .then((data) => {
-            // console.log(data.data == false)
             if (data.data == false) {
                 error.innerText = 'No Phone Found'
             }
@@ -21,13 +19,10 @@ const searchClick = () => {
                 showPhoneDetails(data.data)
             }
         })
-
-
 }
 const showPhoneDetails = (phones) => {
-    // console.log(phones)
     const first20Phones = phones.slice(0, 20)
-    for (const phone of first20Phones) {
+    first20Phones.forEach(phone => {
         console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col-lg-4')
@@ -35,17 +30,17 @@ const showPhoneDetails = (phones) => {
         div.classList.add('mb-5')
         div.classList.add('mt-5')
         div.innerHTML = `
-        <div class="card mx-auto" style="width: 18rem;">
-            <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-               <h5 class="card-title">Phone-Name: ${phone.phone_name}</h5>
-               <p class="card-text">Brand: ${phone.brand}</p>
-               <button onclick="phoneDetail('${phone.slug}')" href="#" class="btn btn-primary">See Detail </button>
+            <div class="card mx-auto" style="width: 18rem;">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                   <h5 class="card-title">Phone-Name: ${phone.phone_name}</h5>
+                   <p class="card-text">Brand: ${phone.brand}</p>
+                   <button onclick="phoneDetail('${phone.slug}')" href="#" class="btn btn-primary">See Detail </button>
+                </div>
             </div>
-        </div>
-        `
+            `
         main.appendChild(div)
-    }
+    })
 }
 const phoneDetail = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
@@ -61,14 +56,14 @@ const setDetail = (info) => {
 
     if (info.releaseDate == '' || info.releaseDate == null) {
         div.innerHTML = `
-        <div class="card mx-auto" style="width: 18rem;">
+        <div class="card mx-auto" style="width: 25rem;">
             <img src="${info.image}" class="card-img-top" alt="...">
             <div class="card-body">
-               <p id="noRelease" class="card-text text-success">No release Date Found</p>
-               <p class="card-text">Storage: ${info.mainFeatures.storage}</p>
-               <p class="card-text">DisplaySize: ${info.mainFeatures.displaySize}</p>
-               <p class="card-text">Sensors: ${info.mainFeatures.sensors}</p>
-               <p class="card-text">Others: WLAN: ${info.others.WLAN}, Bluetooth: ${info.others.Bluetooth}</p>
+               <h5 id="noRelease" class="card-text text-success"><b>No release Date Found</b></h5>
+               <p class="card-text"><b>Storage: </b>${info.mainFeatures.storage}</p>
+               <p class="card-text"><b>DisplaySize: </b>${info.mainFeatures.displaySize}</p>
+               <p class="card-text"><b>Sensors: </b>${info.mainFeatures.sensors}</p>
+               <p class="card-text"><b>Others: WLAN: </b>${info?.others?.WLAN}, Bluetooth: ${info?.others?.Bluetooth}</p>
             </div>
         </div>
         `
@@ -76,20 +71,16 @@ const setDetail = (info) => {
     }
     else {
         div.innerHTML = `
-        <div class="card mx-auto" style="width: 18rem;">
+        <div class="card mx-auto" style="width: 25rem;">
             <img src="${info.image}" class="card-img-top" alt="...">
             <div class="card-body">
-               <p id="noRelease" class="card-text">${info?.releaseDate}</p>
-               <p class="card-text">Storage: ${info.mainFeatures.storage}</p>
-               <p class="card-text">DisplaySize: ${info.mainFeatures.displaySize}</p>
-               <p class="card-text">Others: WLAN: ${info?.others?.WLAN}, Bluetooth: ${info?.others?.Bluetooth}</p>
+               <h5 id="noRelease" class="card-text">${info?.releaseDate}</h5>
+               <p class="card-text"><b>Storage: </b> ${info.mainFeatures.storage}</p>
+               <p class="card-text"><b>DisplaySize: </b>${info.mainFeatures.displaySize}</p>
+               <p class="card-text"><b>Others: WLAN: </b>${info?.others?.WLAN}, Bluetooth: ${info?.others?.Bluetooth}</p>
             </div>
         </div>
         `
         detail.appendChild(div)
     }
-
-
 }
-
-// info.others.WLAN
